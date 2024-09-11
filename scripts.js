443fee0e68daee0b3ef99e66c5a49f91cf9db488
Davi Lorena <davilorena2016@gmail.com>
2024-09-10 21:35:06 -0300

    adding list of objectives

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = { // exemplo/padrão a ser seguido
     value: 'Estudar 8H por dia',
@@ -25,6 +25,35 @@ metas.push({
     value: meta, checked: false
 }
 )
+
+}
+
+const listarMetas = async () => {
+const respostas = await checkbox({
+    message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
+    choices: [...metas],
+    instructions: false,
+})
+
+if(respostas.length == 0) { // validação no padrão 
+    console.log("Nenhuma meta selecionada...")
+    return
+}
+
+metas.forEach((m) => {
+    m.checked = false 
+})
+
+respostas.forEach(metas, (resposta) => {
+const meta = metas.find((m) => {
+    return m.value = resposta
+})
+
+meta.checked = true 
+
+console.log("Meta(s) marcada(s) como concluída(s)!!")
+
+})
 
 }
 
@@ -56,12 +85,14 @@ await cadastrarMeta()
 console.log(metas)
 break
 case "listar":
+    await listarMetas()
     break
     case "sair": 
     console.log("Até a próxima!")
     return
     }
  }
+
 }
 
 start()
