36c6b4ec21230400f82272294249a2d9af598851
Davi Lorena <davilorena2016@gmail.com>
2024-09-11 08:37:43 -0300

    adding checked in objectives

--- scripts.js
+++ scripts.js
@@ -2,15 +2,13 @@ const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = { // exemplo/padrão a ser seguido
     value: 'Estudar 8H por dia',
-    checked: true
+    checked: false,
 }
 
-let metas = [ meta
-
-]
+let metas = [ meta ]
 
 const cadastrarMeta = async () => {
-const meta = await input({message: "Digite a meta"})
+const meta = await input({message: "Digite a meta:"})
 
  /* Validação de inputs:
  Length significa mais de um dígito e se eles forem vazios o "if" entra em ação (baseado nas condições que eu coloquei nesse caso em específico) e o que for escrito dentro dos parênteses acontecerá, pois com o valor true para essa condição, o input está inválido. Vale destacar, novamente, a importância do "return". 
@@ -28,8 +26,9 @@ metas.push({
 
 }
 
+
 const listarMetas = async () => {
-const respostas = await checkbox({
+    const respostas = await checkbox({
     message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
     choices: [...metas],
     instructions: false,
@@ -44,15 +43,33 @@ metas.forEach((m) => {
     m.checked = false 
 })
 
-respostas.forEach(metas, (resposta) => {
+respostas.forEach((resposta) => {
 const meta = metas.find((m) => {
     return m.value = resposta
 })
 
+
 meta.checked = true 
+
+})
 
 console.log("Meta(s) marcada(s) como concluída(s)!!")
 
+}
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+if(realizadas.length == 0) {
+    console.log("Não existem mets realizadas :(")
+    return
+}
+
+await select ({
+    message: "Metas realizadas",
+    choices: [...realizadas]
 })
 
 }
@@ -72,6 +89,10 @@ const start = async () => {
                 name: "Listar metas",
                 value: "listar"
             },
+            {
+                name: "Metas realizadas",
+                value: "realizadas"
+            },
             { 
                 name: "Sair",
                 value: "sair"
@@ -87,6 +108,9 @@ break
 case "listar":
     await listarMetas()
     break
+    case "realizadas":
+        await metasRealizadas()
+        break
     case "sair": 
     console.log("Até a próxima!")
     return
